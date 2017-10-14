412d816283033d624cbfd868574d9b36795fca11
mspaint09 <mspaint09@gmail.com>
2017-10-13 18:21:39 -0700

    Delete b4.sql

--- Database Connections/b4.sql
+++
@@ -1,6 +0,0 @@
-select c.code, c.name, c.population, String_agg(n.name::text,',') neighbor_list, sum(n.population) 
-From country c
-JOIN borders b ON UPPER(c.code)=UPPER(b.country1)
-JOIN country n ON UPPER(b.country2)=UPPER(n.code)
-GROUP By c.code, c.name, c.population;
-
