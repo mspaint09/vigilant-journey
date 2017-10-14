50292bc0543a17b729fd3807951461ef5cdb3c8a
mspaint09 <mspaint09@gmail.com>
2017-10-13 18:33:48 -0700

    Create airbus.js

---
+++ Javascript/airbus.js
@@ -0,0 +1,8 @@
+var copyright_info = "SM Copyright 2017"
+var A380 = 555
+var tix_sold = 55
+var complementary = 100
+var total_available_seats = A380 - tix_sold - complementary
+
+console.log(copyright_info)
+console.log(total_available_seats)
