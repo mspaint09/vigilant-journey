d3d83c1e2078ca9d85bab7ef3f7ebde2a78cccaa
mspaint09 <mspaint09@gmail.com>
2017-10-13 18:21:45 -0700

    Delete b3.sql

--- Database Connections/b3.sql
+++
@@ -1,7 +0,0 @@
--- For each country, list the city (or cities) with the largest population. 
--- Select the country code of the country, followed by the province that the city is located in, along with the city name.
-
-SELECT COUNTRY, PROVINCE, NAME, POPULATION FROM CITY 
-WHERE (COUNTRY, POPULATION) IN (
-								SELECT COUNTRY, MAX(POPULATION) FROM CITY GROUP BY COUNTRY ORDER BY COUNTRY
-								);
